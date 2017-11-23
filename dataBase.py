105924c7a7d9242deb0391405b0ceabc6f6f1033
ArnaudPalgen <zig.count@gmail.com>
2017-11-23 10:26:50 +0100

    second commit

---
+++ dataBase.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+dataBase = sqlite3.connect('chinook.db')
+cursor=dataBase.cursor()
+
+finding=cursor.execute("""SELECT Name, Composer from tracks""" )
+for rows in finding:
+	print(rows)
+dataBase.close()
